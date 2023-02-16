49867f6a828ca15a61418eaeb57c5491ce02e4d1
David Ordás <3125580+davorpa@users.noreply.github.com>
2023-02-16 01:02:07 +0100

    refactor: move created instances to a new module file
    It acts as singleton DI context to also facilitate tests

---
+++ src/context/index.js
@@ -0,0 +1,13 @@
+import { OMDbClient, OMDbJSONClient } from '@services/omdb'
+
+/** @type {import('../services/omdb').OMDbAbstractClient} */
+let omdbClient
+if (import.meta.env.VITE_OMDB_CLIENT_STRATEGY === 'json') {
+  /** @type {import('../services/omdb').OMDbJSONClient} */
+  omdbClient = new OMDbJSONClient(import.meta.env.VITE_OMDB_APIKEY)
+} else {
+  /** @type {import('../services/omdb').OMDbClient} */
+  omdbClient = new OMDbClient(import.meta.env.VITE_OMDB_APIKEY)
+}
+
+export { omdbClient }
--- src/context/omdb/client.jsx
+++ src/context/omdb/client.jsx
@@ -1,16 +1,6 @@
 import React from 'react'
 import reactPropTypes from 'prop-types'
-import { OMDbClient, OMDbJSONClient } from '@services/omdb'
-
-/** @type {import('../../services/omdb').OMDbAbstractClient} */
-let omdbClient
-if (import.meta.env.VITE_OMDB_CLIENT_STRATEGY === 'json') {
-  /** @type {import('../../services/omdb').OMDbJSONClient} */
-  omdbClient = new OMDbJSONClient(import.meta.env.VITE_OMDB_APIKEY)
-} else {
-  /** @type {import('../../services/omdb').OMDbClient} */
-  omdbClient = new OMDbClient(import.meta.env.VITE_OMDB_APIKEY)
-}
+import { omdbClient } from '@/context'
 
 /** A React Context that provides access to the OMDbClient */
 export const OMDbClientContext = React.createContext(omdbClient)
