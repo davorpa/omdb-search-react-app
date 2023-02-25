9ba4d113d31c674f342c4143240b3dee3c693c7c
David Ordás <3125580+davorpa@users.noreply.github.com>
2023-02-25 05:42:17 +0100

    docs: document useFocuserEffect hook

--- src/hooks/useFocuserEffect.js
+++ src/hooks/useFocuserEffect.js
@@ -1,15 +1,29 @@
 import { useEffect, useRef } from 'react'
 
+/**
+ * Hook to be used in a component that needs to focus on a specific
+ * DOM element with errors or scroll to the DOM element that contains
+ * the component that consumes the datasource list.
+ *
+ * @function useFocuserEffect
+ * @memberof module:hooks
+ * @param {Array<*>} list - Datasource list used to inspect for focus or scroll
+ * @returns {Array<import('react').Ref<Element>>} - Refs to be used in the component.
+ *      One for the DOM element that needs to be scrolled to when the list has items
+ *      and another for the that which needs to be focused when is empty.
+ */
 export function useFocuserEffect(list) {
   const scrollerRef = useRef(null)
   const focusedRef = useRef(null)
 
   const effectMonitorRef = useRef(true)
   useEffect(() => {
+    // only when list datasource changes, not when component is mounted
     if (effectMonitorRef.current) {
       effectMonitorRef.current = false
       return
     }
+    // fire actions
     if (list?.length) {
       scrollerRef.current.scrollIntoView({
         behavior: 'smooth',
@@ -20,5 +34,6 @@ export function useFocuserEffect(list) {
       focusedRef.current.focus()
     }
   }, [list])
+
   return [scrollerRef, focusedRef]
 }
